0082f42ca01a30e2e25e64cb82f3acb34bc9f611
babyblue4u <babyblue4u8989@yahoo.com>
2016-02-07 19:54:21 -0500

    Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,1 +1,15 @@
+data_full <- read.csv("./household_power_consumption.txt", header=TRUE, sep=';', na.strings="?",
+                      nrows=2075260, check.names=FALSE, stringsAsFactors=FALSE, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+hist(data$Global_active_power, main="Global Active Power",
+     xlab="Global Active Power (kilowatts", ylab="Frequency", col="Red")
+
+dev.copy(png, file="plot.png, height=480, width=480")
+dev.off()
